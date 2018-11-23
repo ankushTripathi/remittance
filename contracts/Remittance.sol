bd6e71152a61eae942bab6b87fe807ef97e569a8
ankushTripathi <ankushtripathi0996@yahoo.com>
2018-11-23 12:43:56 +0530

    single hash method

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -11,12 +11,12 @@ contract Remittance {
     mapping(bytes32 => PaymentStruct) paymentStructs;
 
     event LogFundsDeposited(bytes32 indexed _hash, address indexed alice, uint amount);
-    event LogFundsWithdrawn(bytes32 indexed _hash,address indexed carol, uint amount);
+    event LogFundsWithdrawn(bytes32 indexed _hash, address indexed carol, uint amount);
 
 
-    function hashing(address a,bytes32 b) public view returns(bytes32){
+    function hashing(address a, bytes32 b) public view returns(bytes32){
         
-        return keccak256(abi.encodePacked(a,b,address(this)));
+        return keccak256(abi.encodePacked(a, b, address(this)));
     }
 
     function deposit(bytes32 _hash) public payable{
@@ -34,7 +34,7 @@ contract Remittance {
 
     function withdraw(bytes32 password) public {
 
-        bytes32 payment_hash  = keccak256(abi.encodePacked(msg.sender,password,address(this)));
+        bytes32 payment_hash  = hashing(msg.sender, password);
 
         PaymentStruct storage pay = paymentStructs[payment_hash];
 
@@ -42,7 +42,7 @@ contract Remittance {
         require(amount > 0);
         
         pay.amount = 0;
-        emit LogFundsWithdrawn(payment_hash,msg.sender,amount);
+        emit LogFundsWithdrawn(payment_hash, msg.sender, amount);
         msg.sender.transfer(amount);
     }
 }
